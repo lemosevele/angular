3c0b12dc3bd97028becfaae095ce3d2cdcb5e25c
Evele <lemosevele@gmail.com>
2023-08-02 16:54:04 -0300

    chamada para API

---
+++ rxjs-alura/src/app/service/livro.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LivroService {
+
+  private readonly API = 'https://www.googleapis.com/books/v1/volumes';
+
+  constructor(private http: HttpClient) { }
+
+  buscar(valorDigitado: string): Observable<Item[]> {
+    const params = new HttpParams().append('q', valorDigitado);
+    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
+      tap(retorno =>
+        console.log('tap - Fluxo antes do map: ', retorno)
+      ),
+      map(retorno => 
+        retorno.items
+      ),
+      tap(
+        retorno =>
+        console.log('tap - Fluxo depois do map: ', retorno)
+      )
+    );
+  }
+
+}
--- rxjs-alura/src/app/views/lista-livros/lista-livros.component.ts
+++ rxjs-alura/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
+  campoBusca: string = '';
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private livroService: LivroService,
+  ) { }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  buscarLivros() {
+    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
+      next: retorno => console.log(retorno),
+      error: erro => console.error(erro),
+      complete: () => console.log('Observable completado')
+    });
+  }
 
 }
 
