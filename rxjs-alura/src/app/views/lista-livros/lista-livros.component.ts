66250a0eeed6f12ea3b985d3fbdcbf39af1984bf
Evele <lemosevele@gmail.com>
2023-09-13 22:00:51 -0300

    otimizando busca

--- rxjs-alura/src/app/views/lista-livros/lista-livros.component.ts
+++ rxjs-alura/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { Subscription, map, switchMap, filter, debounceTime, distinctUntilChanged } from 'rxjs';
 import { Item, Livro, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300;
 
 @Component({
   selector: 'app-lista-livros',
@@ -11,27 +14,38 @@ import { LivroService } from 'src/app/service/livro.service';
 })
 export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: Livro[];
+  // listaLivros: Livro[];
   livro: Livro;
-  campoBusca: string = '';
-  subscription: Subscription;
+  campoBusca = new FormControl();
+  // subscription: Subscription;
 
   constructor(
     private livroService: LivroService,
   ) { }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    // this.subscription.unsubscribe();
+    // utilizando o '| async' não precisa fazer subscriber/unsubscriber manualmente
   }
 
-  buscarLivros() {
-    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
-      next: items => {
-        this.listaLivros = this.livrosResultadoParaLivros(items);
-      },
-      error: erro => console.error(erro)
-    });
-  }
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSA),
+      filter( valorDigitado => valorDigitado.length >= 3),
+      distinctUntilChanged(),
+      switchMap( valorDigitado => this.livroService.buscar(valorDigitado) ),
+      map( items => this.livrosResultadoParaLivros(items) )
+      // o retorno foi atribuído a variável listaLivros no HTML
+    )
+
+  // buscarLivros() {
+  //   this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
+  //     next: items => {
+  //       this.listaLivros = this.livrosResultadoParaLivros(items);
+  //     },
+  //     error: erro => console.error(erro)
+  //   });
+  // }
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => { 
